85d829caa455053ff581c6ef81a2338f8851bdb2
HulinWangGitHub <hulinwang.ustc@gmail.com>
2021-03-24 08:42:51 +0800

    modify analyze.py

--- data_analysis/Analyze_Jadepixtxt.py
+++ data_analysis/Analyze_Jadepixtxt.py
@@ -8,11 +8,19 @@ import time
 import numpy as np
 from rootUtil3 import waitRootCmdX
 
-def test():
-    print("test")
-    #filename = "data_rs_test.txt"
-    filename = "/home/hulin/Workdir/JadePix/Data/data/data_rs.txt"
-    outfile = TFile('outth2f.root','recreate')
+def test(filename="../Data/data/data_rs.txt", outrootname = "./outth2f.root"):
+
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-i", "--input", dest="inputTxtFileName", help="the name of input txt file")
+    parser.add_option("-o", "--out", dest="outputRootFileName", help="the name of output root file")
+    (options, args) = parser.parse_args()
+    if(options.inputTxtFileName): filename = options.inputTxtFileName
+    if(options.outputRootFileName): outrootname = options.outputRootFileName
+
+    print(f"filename: {filename}, outrootname: {outrootname}")
+
+    outfile = TFile(outrootname,'recreate')
     with open(filename,'r') as fin:
         lines = fin.readlines()
         lineindex = 0
@@ -20,6 +28,7 @@ def test():
         framestart = False
         frameend = False
         c1 = TCanvas("c1","c1",800,600)
+        c2 = TCanvas("c2","c2",800,600)
         for line in lines:
 #             print(lineindex)
             lineindex +=1
@@ -39,13 +48,30 @@ def test():
                 print(th2tag)
                 #th2 = TH2F(th2tag,th2tag,512,0,512,192,0,192);
                 th2 = TH2F(th2tag,th2tag,192,0,192,512,0,512);
+                th2.GetXaxis().SetTitle("Column")
+                th2.GetYaxis().SetTitle("Row")
                 framenumber += 1
             elif flag == 0 and framestart:
                 frameend = True
                 framestart = False
                 print(f"framenumber: {raw16}")
+                c1.cd()
                 th2.Draw("colz")
                 c1.Update()
+               
+                xbinlow = th2.FindFirstBinAbove(0.1, 1)
+                xbinhigh = th2.FindLastBinAbove(0.1, 1)
+                ybinlow = th2.FindFirstBinAbove(0.1, 2)
+                ybinhigh = th2.FindLastBinAbove(0.1, 2)
+                print(f"xbinlow {xbinlow}, xbinhigh {xbinhigh}, ybinlow {ybinlow}, ybinhigh {ybinhigh}")
+             
+                th2clone = th2.Clone("th2clone")
+                c2.cd()
+                th2clone.GetXaxis().SetRangeUser(xbinlow-5, xbinhigh+5)
+                th2clone.GetYaxis().SetRangeUser(ybinlow-5, ybinhigh+5)
+                th2clone.Draw("colz")
+                c2.Update()
+
                 outfile.cd()
                 th2.Write()
 #                 a = input("aaa")
@@ -69,10 +95,25 @@ def test():
                 print(f"end of file")
                 th2.Draw("colz")
                 c1.Update()
+
+                xbinlow = th2.FindFirstBinAbove(0.1, 1)
+                xbinhigh = th2.FindLastBinAbove(0.1, 1)
+                ybinlow = th2.FindFirstBinAbove(0.1, 2)
+                ybinhigh = th2.FindLastBinAbove(0.1, 2)
+                print(f"xbinlow {xbinlow}, xbinhigh {xbinhigh}, ybinlow {ybinlow}, ybinhigh {ybinhigh}")
+
+                th2clone = th2.Clone("th2clone")
+                c2.cd()
+                th2clone.GetXaxis().SetRangeUser(xbinlow-5, xbinhigh+5)
+                th2clone.GetYaxis().SetRangeUser(ybinlow-5, ybinhigh+5)
+                th2clone.Draw("colz")
+                c2.Update()
+
                 outfile.cd()
                 th2.Write()
                 a = input("aaa")
     outfile.Close()
+
 
 if __name__ == '__main__':
     gROOT.LoadMacro("AtlasStyle.C")
