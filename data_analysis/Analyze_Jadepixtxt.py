008c95c7eb44c4b217725983a16e6166aa2492df
sdong <s.dong@mails.ccnu.edu.cn>
2021-03-24 20:07:16 +0800

    debug tring and bug fixing

--- data_analysis/Analyze_Jadepixtxt.py
+++ data_analysis/Analyze_Jadepixtxt.py
@@ -35,7 +35,7 @@ def test(filename="../Data/data/data_rs.txt", outrootname = "./outth2f.root"):
 #             if lineindex > 20:
 #                 break
             raw16 = int(line, 16)
-            flag = (raw16 >> 30)
+            flag = (raw16 >> 23)
 #             print(hex(raw16), flag)
             if flag !=1 and (not framestart) and (not frameend):
 #                 print(flag, framestart, frameend)
@@ -115,8 +115,132 @@ def test(filename="../Data/data/data_rs.txt", outrootname = "./outth2f.root"):
     outfile.Close()
 
 
+def testresidual(filename="../Data/data/data_rs.txt", outrootname = "./outth1f.root", laserxpoi=0.700):
+
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-i", "--input", dest="inputTxtFileName", help="the name of input txt file")
+    parser.add_option("-o", "--out", dest="outputRootFileName", help="the name of output root file")
+    parser.add_option("-x", "--x", dest="laserX", help="laser x position")
+    (options, args) = parser.parse_args()
+    if(options.inputTxtFileName): filename = options.inputTxtFileName
+    if(options.outputRootFileName): outrootname = options.outputRootFileName
+    if(options.laserX): laserxpoi = float(options.laserX)
+
+    print(f"filename: {filename}, outrootname: {outrootname}")
+
+    outfile = TFile(outrootname,'recreate')
+    th1resi = TH1F("th1resi","th1resi",100000,0,1000)
+    th1center = TH1F("th1center","th1center",100000,0,10000)
+    with open(filename,'r') as fin:
+        lines = fin.readlines()
+        lineindex = 0
+        framenumber = 0
+        framestart = False
+        frameend = False
+        c1 = TCanvas("c1","c1",800,600)
+        c2 = TCanvas("c2","c2",800,600)
+        for line in lines:
+#             print(lineindex)
+            lineindex +=1
+#             if lineindex > 20:
+#                 break
+            raw16 = int(line, 16)
+            flag = (raw16 >> 23)
+#             print(hex(raw16), flag)
+            if flag !=1 and (not framestart) and (not frameend):
+#                 print(flag, framestart, frameend)
+                continue
+
+            if flag == 1: 
+                framestart = True
+                frameend = False
+                th2tag = f"TH2_{framenumber}"
+                print(th2tag)
+                #th2 = TH2F(th2tag,th2tag,512,0,512,192,0,192);
+                th2 = TH2F(th2tag,th2tag,192,0,192,512,0,512);
+                th2.GetXaxis().SetTitle("Column")
+                th2.GetYaxis().SetTitle("Row")
+                framenumber += 1
+            elif flag == 0 and framestart:
+                frameend = True
+                framestart = False
+                print(f"framenumber: {raw16}")
+                c1.cd()
+                #th2.Draw("colz")
+                c1.Update()
+               
+                xbinlow = th2.FindFirstBinAbove(0.1, 1)
+                xbinhigh = th2.FindLastBinAbove(0.1, 1)
+                ybinlow = th2.FindFirstBinAbove(0.1, 2)
+                ybinhigh = th2.FindLastBinAbove(0.1, 2)
+                print(f"xbinlow {xbinlow}, xbinhigh {xbinhigh}, ybinlow {ybinlow}, ybinhigh {ybinhigh}")
+                xbincenter = (xbinlow+xbinhigh)*0.5*23.
+                residuX =  xbincenter - (0.7-laserxpoi)*1000-100.*23
+                th1resi.Fill(residuX)
+                th1center.Fill(xbincenter)
+                print(f"xbincenter {xbincenter}, residuX {residuX}")
+                
+                th2clone = th2.Clone("th2clone")
+                c2.cd()
+                th2clone.GetXaxis().SetRangeUser(xbinlow-5, xbinhigh+5)
+                th2clone.GetYaxis().SetRangeUser(ybinlow-5, ybinhigh+5)
+                #th2clone.Draw("colz")
+                c2.Update()
+
+                outfile.cd()
+                #th2.Write()
+#                 a = input("aaa")
+                #waitRootCmdX(f"Eventnumber: {framenumber}, framenumber: {raw16}")
+
+            elif flag == 2 and framestart and (not frameend): #data
+                sec = (raw16 >> 16) & 0x3 #0-3
+                data = (raw16 & 0xFFFF)
+                datarow = (data >> 7) #0-511
+                datablk = (data >> 3) & 0xF #0-15
+                databit2 = (data >> 2) & 0x1
+                databit1 = (data >> 1) & 0x1
+                databit0 = (data & 0x1)
+                datacolumn = sec * 48 + datablk * 3 
+                print(hex(raw16), flag, sec, bin(data), datarow, datablk, databit0, databit1, databit2, datacolumn)
+                th2.SetBinContent(datacolumn, datarow, databit0);
+                th2.SetBinContent(datacolumn+1, datarow, databit1);
+                th2.SetBinContent(datacolumn+2, datarow, databit2);
+   
+            if lineindex == len(lines):
+                print(f"end of file")
+                #th2.Draw("colz")
+                c1.Update()
+
+                xbinlow = th2.FindFirstBinAbove(0.1, 1)
+                xbinhigh = th2.FindLastBinAbove(0.1, 1)
+                ybinlow = th2.FindFirstBinAbove(0.1, 2)
+                ybinhigh = th2.FindLastBinAbove(0.1, 2)
+                print(f"xbinlow {xbinlow}, xbinhigh {xbinhigh}, ybinlow {ybinlow}, ybinhigh {ybinhigh}")
+
+                th2clone = th2.Clone("th2clone")
+                c2.cd()
+                th2clone.GetXaxis().SetRangeUser(xbinlow-5, xbinhigh+5)
+                th2clone.GetYaxis().SetRangeUser(ybinlow-5, ybinhigh+5)
+                #th2clone.Draw("colz")
+                c2.Update()
+
+                outfile.cd()
+                #th2.Write()
+                #waitRootCmdX(f"endoffile")
+    c1.cd()
+    #th1resi.Draw()
+    c2.cd()
+    #th1center.Draw()
+    #waitRootCmdX(f"residual and center")
+    th1resi.Write()
+    th1center.Write()
+    outfile.Close()
+
+
 if __name__ == '__main__':
     gROOT.LoadMacro("AtlasStyle.C")
     gROOT.ProcessLine("SetAtlasStyle()")
     gStyle.SetPalette(55)
-    test()
+    #test()
+    testresidual()
---
+++ data_analysis/run_Analyze.sh
@@ -0,0 +1,5 @@
+./Analyze_Jadepixtxt.py  -i ../data/data_rs_x0700.txt   -o test_x0700.root -x 0.700 > log_x0700 2>&1&
+./Analyze_Jadepixtxt.py  -i ../data/data_rs_x0701.txt   -o test_x0701.root -x 0.701 > log_x0701 2>&1&
+./Analyze_Jadepixtxt.py  -i ../data/data_rs_x0702.txt   -o test_x0702.root -x 0.702 > log_x0702 2>&1&
+./Analyze_Jadepixtxt.py  -i ../data/data_rs_x0703.txt   -o test_x0703.root -x 0.703> log_x0703 2>&1&
+./Analyze_Jadepixtxt.py  -i ../data/data_rs_x0704.txt   -o test_x0704.root -x 0.704> log_x0704 2>&1&
