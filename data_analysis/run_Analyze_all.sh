4c401362db7ea9ba43683db8c875251cd39092d5
Yunpeng <yplu@ihep.ac.cn>
2021-03-25 15:20:08 +0800

    update analysis

--- data_analysis/run_Analyze_all.sh
+++ data_analysis/run_Analyze_all.sh
@@ -1,10 +1,13 @@
-for i in $(seq 10 30); do
+mkdir -p Logfiles
+mkdir -p Outfiles
+
+for i in $(seq 10 28); do
     ftag="7${i}"
-    ./Analyze_Jadepixtxt.py  -i ../data/data_rs_x0${ftag}.txt   -o test_x0${ftag}.root -x 0.${ftag} > log_x0${ftag}  2>&1&
+    ./Analyze_Jadepixtxt.py  -i ../data/data_rs_0325_x0${ftag}.txt   -o Outfiles/test_0325_x0${ftag}.root -x 0.${ftag} > Logfiles/log_0325_x0${ftag}  2>&1&
 done
 
 
 for i in $(seq 0 9); do
     ftag="70${i}"
-    ./Analyze_Jadepixtxt.py  -i ../data/data_rs_x0${ftag}.txt   -o test_x0${ftag}.root -x 0.${ftag} > log_x0${ftag}  2>&1&
+    ./Analyze_Jadepixtxt.py  -i ../data/data_rs_0325_x0${ftag}.txt   -o Outfiles/test_0325_x0${ftag}.root -x 0.${ftag} > Logfiles/log_0325_x0${ftag}  2>&1&
 done
