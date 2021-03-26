334d80726d1477de6d338085697296360503a198
HulinWangGitHub <hulinwang.ustc@gmail.com>
2021-03-26 17:33:29 +0800

    add drawXY

---
+++ data_analysis/draw_XY.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import os, sys
+import glob
+from array import array
+from ROOT import TCanvas, TGraphErrors, TLine, gROOT, TGaxis, gPad, TLegend, TH1F, TH1D, TChain, gDirectory, TFile
+from datetime import datetime
+import time
+import numpy as np
+from ROOT import kBlue, kRed, kBlack, kGreen
+import math
+from rootUtil3 import waitRootCmdX
+
+
+def test(filename="total_test.root"):
+    filename = sys.argv[1]
+    f1 = TFile(filename)
+    for key1 in f1.GetListOfKeys():
+        tp1 = f1.Get(key1.GetName())
+        print(f"{key1.GetName()} {tp1.GetRMS()}")
+
+def GetEntries(filename, histname):
+    f1 = TFile(filename)
+    print(filename, f1.Get("th1resiXprojXall").GetEntries()) 
+
+
+def draw_X():
+    filenamesX = glob.glob("Outfiles/test_rs_0326_tune94p1_x*.root")
+    filenamesX = sorted(filenamesX, key=lambda x:x)
+
+    grs = TGraphErrors()
+    index = 0
+    for filename in filenamesX:
+        X = float(os.path.basename(filename).split('.')[0][-4:])
+        NEntries = GetEntries(filename, "th1resiXprojXall")
+        print(filename, X, NEntries)
+        grs.SetPoint(index, X, NEntries)
+        grs.SetPointError(index, 0, 0)
+        index += 1
+
+    c1 = TCanvas("c1","c1",0,0,800,600)
+    grs.Draw("APL")
+    grs.GetXaxis().SetTitle("Laser Position X (#mum)")
+    grs.GetYaxis().SetTitle("Entries")
+    c1.Update()
+    os.makedirs("./Plots", exist_ok=True)
+    c1.Print("./Plots/EntriesVSX.pdf")
+    waitRootCmdX()
+
+
+
+if __name__ == '__main__':
+    #test()
+    draw_X()
